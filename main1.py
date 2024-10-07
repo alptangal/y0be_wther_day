3a86994efd493a967a291c71b63c7c733b7dd58e
alptangal <alptangal@gmail.com>
2024-10-07 23:13:30 +0700

    .

--- main1.py
+++ main1.py
@@ -33,6 +33,6 @@ options.add_argument("--start-maximized")
 options.add_argument("--disable-extensions")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome( options=options)
 driver.get("https://www.reddit.com")
 print(str(driver))
